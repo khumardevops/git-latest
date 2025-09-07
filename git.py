817e3998d49bea42ec34a1e30940e36505083c56
khumardevops <atchuthaaj@gmail.com>
2025-09-07 10:53:19 +0000

    git commands added

---
+++ git.py
@@ -0,0 +1,12 @@
+git init
+git add .
+git commit -m "commit message"
+git push aliasname master
+git config --global user.name "khumardevops"
+git config --global user.email "atchuthaaj@gmail.com"
+git remote -v
+git log
+git show
+git reset
+git revert
+
